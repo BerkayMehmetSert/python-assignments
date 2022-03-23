2b24ef2455049fe5fd89ff0aee37a56b205f47f9
BerkayMehmetSert <berkaymehmetsert@gmail.com>
2022-03-23 20:17:22 +0300

    assignments tutorial

---
+++ assignments-demo.py
@@ -0,0 +1,21 @@
+x = 2
+y = 5
+z = 10
+
+# 1- What is the difference between the product of the 2 numbers you received from the user and the sum of x, y, z?
+a = int(input("Enter a number: "))
+b = int(input("Enter a number: "))
+result = a*b - (x+y+z)
+print(result)
+
+# 2-  Calculate the  y modulus x.
+result = y // x 
+print(result)
+
+# 3-  Calculate the product of x and y.
+result = x * y
+print(result)
+
+# 4-  Calculate the sum of x, y, and z.
+result = x + y + z
+print(result)
